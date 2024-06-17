4bfa79f12ef2cdd1c9b2985ea051375dec77a48c
Vanessa Di Bella <vanessa@di-bella.de>
2024-06-17 21:52:02 +0200

    :add solution

---
+++ Woche2/Wiederholung/javascript/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,35 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. 
+Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const title = "How to code with JS";
+const text = "We will learn everything with neuefische";
+let numberOfLikes = 232;
+const user = "Vanessa";
+const isReported = "true";
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. 
+Increase the likes by one and log the amount of likes again. 
+Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log(title);
+console.log(text);
+console.log(numberOfLikes);
+console.log(user);
+console.log(isReported);
+numberOfLikes += 10;
+console.log(numberOfLikes);
+// --^-- write your code here --^--
